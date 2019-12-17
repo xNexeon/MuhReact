575c08f71d8eb6ff1d5dc1013baa90bba05dce72
Niko Wisniewski <niko02xbox@gmail.com>
2019-12-17 11:09:37 +0000

    Nothing

---
+++ muhreact/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Home() {
+  return (
+    <div className="">
+      <h1>Oi</h1>
+    </div>
+  );
+}
+
+export default Home;
