1fb1700f35d95531bc0df0dd463191221061e869
Niko Wisniewski <niko02xbox@gmail.com>
2020-01-07 14:55:51 +0000

    added stich

--- muhreact/src/components/Home.js
+++ muhreact/src/components/Home.js
@@ -2,6 +2,17 @@ import React from 'react';
 import './Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'
+
+function initializeAndLogin() {
+  const client = Stitch.initializeDefaultAppClient('zforum-pvpwj');
+  client.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+    console.log('Connected to Stitch Sucessfully')
+  });
+}
+window.onload = initializeAndLogin;
+
+
 
 function Home() {
   return (
